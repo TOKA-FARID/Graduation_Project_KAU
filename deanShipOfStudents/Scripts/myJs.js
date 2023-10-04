cff20e496f1441f98698d6c3a8766d2041639b0f
TOKA-FARID <140805219+TOKA-FARID@users.noreply.github.com>
2023-10-04 06:45:45 +0300

    Upload Graduation Project

---
+++ deanShipOfStudents/Scripts/myJs.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    
+
+    $('#IsAssigned').change( function () {
+        
+        if (this.checked) {
+            
+            $('#TrainerId').removeClass("d-none");
+            $('#lblTrainer').removeClass("d-none");
+
+        } else {
+            $('#TrainerId').addClass("d-none");
+            $('#lblTrainer').addClass("d-none");
+
+        }
+    });
+
+    $("#inputSearch").keyup(function () {
+        var input, filter, table, tr, tdActivityName, tdLocation, tdtype, i, txtNameValue, txtLocationValue, txtTypeValue;
+        input = document.getElementById("inputSearch");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("tableSearch");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            tdActivityName = tr[i].getElementsByTagName("td")[0];
+            tdLocation = tr[i].getElementsByTagName("td")[1];
+            tdtype = tr[i].getElementsByTagName("td")[2];
+           
+
+            if (tdActivityName || tdLocation || tdtype ) {
+                txtNameValue = tdActivityName.textContent || tdActivityName.innerText;
+                txtLocationValue = tdLocation.textContent || tdLocation.innerText;
+                txtTypeValue = tdtype.textContent || tdtype.innerText;
+                
+
+                if (txtNameValue.toUpperCase().indexOf(filter) > -1 || txtLocationValue.toUpperCase().indexOf(filter) > -1 || txtTypeValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    });
+
+    /////////////////////
+});
---
+++ deanShipOfStudents/traineActivityModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace deanShipOfStudents
+{
+    public class traineActivityModel
+    {
+
+        public string ActivityName { get ; set ; }
+        public string Location { get ; set ; }
+        public double AvgAttendance { get ; set ; }
+        public double AvgEvaluation { get ; set; }
+        public string TraineeName { get ; set ; }
+        public string UniverstyId { get ; set ; }
+    }
+}
